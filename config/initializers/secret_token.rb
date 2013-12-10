ad2de7400c0777e89b2659f9d1c083a21358b12a
Melanie Gilman <melanie.gilman@gmail.com>
2013-12-10 13:56:54 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+LanguageMap::Application.config.secret_key_base = 'd0822356dbe20b1b880eeddbf0b30ae702c229c80ba099647f407be0f5acdd6f82b210f9f1f7a612d5daa0cfbb8f63836054a5253df3fe5402902c71c481e1b6'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+LanguageMap::Application.config.session_store :cookie_store, key: '_language_map_session'
