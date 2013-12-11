a1a2e95f85afba2fc81ff3f75a77824d9bf24824
Melanie Gilman <melanie.gilman@gmail.com>
2013-12-10 21:08:43 -0500

    Write method to scope repos with latlong

--- app/models/repository.rb
+++ app/models/repository.rb
@@ -2,4 +2,8 @@ class Repository < ActiveRecord::Base
   validates :url, presence: true, uniqueness: true
   validates :language, presence: true
   validates :location, presence: true
+
+  def self.has_latlong
+    self.where("latitude IS NOT NULL AND longitude IS NOT NULL")
+  end
 end
--- spec/models/repository_spec.rb
+++ spec/models/repository_spec.rb
@@ -1,5 +1,15 @@
 require 'spec_helper'
 
 describe Repository do
-  # pending "add some examples to (or delete) #{__FILE__}"
+  describe ".has_latlong" do
+    let!(:repo_with_latlong)    { FactoryGirl.create(:repository, latitude: 38.9072309, longitude: -77.0364641) }
+    let!(:repo_without_latlong) { FactoryGirl.create(:repository, location: "New York") }
+
+    it "includes repos with latlong and excludes repos without" do
+      has_latlong = Repository.has_latlong
+      expect(has_latlong).to have(1).item
+      expect(has_latlong).to include(repo_with_latlong)
+      expect(has_latlong).not_to include(repo_without_latlong)
+    end
+  end
 end
